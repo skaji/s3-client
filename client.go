8d4483c4697652b2ec3613d4682a1b915b3fbaba
Shoichi Kaji <skaji@cpan.org>
2024-10-26 06:22:56 +0900

    pointerOrNil

--- client.go
+++ client.go
@@ -23,21 +23,16 @@ func (c *Client) ListBuckets(ctx context.Context) (*s3.ListBucketsOutput, error)
 }
 
 func (c *Client) ListObjects(ctx context.Context, bucket string, keyPrefix string) (*s3.ListObjectsV2Output, error) {
-	var keyPrefix2 *string
-	if keyPrefix != "" {
-		keyPrefix2 = aws.String(keyPrefix)
-	}
-	res, err := c.client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
-		Bucket: aws.String(bucket),
-		Prefix: keyPrefix2,
+	return c.client.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
+		Bucket: pointerOrNil(bucket),
+		Prefix: pointerOrNil(keyPrefix),
 	})
-	return res, err
 }
 
 func (c *Client) GetObject(ctx context.Context, bucket string, key string) (io.Reader, func(), error) {
 	res, err := c.client.GetObject(ctx, &s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
+		Bucket: pointerOrNil(bucket),
+		Key:    pointerOrNil(key),
 	})
 	if err != nil {
 		return nil, nil, err
@@ -56,23 +51,27 @@ type PutObjectInput struct {
 }
 
 func (c *Client) PutObject(ctx context.Context, bucket string, key string, input PutObjectInput) error {
-	var contentType *string
-	if input.ContentType != "" {
-		contentType = aws.String(input.ContentType)
-	}
 	_, err := c.client.PutObject(ctx, &s3.PutObjectInput{
-		Bucket:        aws.String(bucket),
-		Key:           aws.String(key),
+		Bucket:        pointerOrNil(bucket),
+		Key:           pointerOrNil(key),
 		Body:          input.Body,
-		ContentLength: aws.Int64(input.ContentLength),
-		ContentType:   contentType,
+		ContentLength: pointerOrNil(input.ContentLength),
+		ContentType:   pointerOrNil(input.ContentType),
 	})
 	return err
 }
 
 func (c *Client) PresignGetObject(ctx context.Context, bucket string, key string) (*signer.PresignedHTTPRequest, error) {
 	return s3.NewPresignClient(c.client).PresignGetObject(ctx, &s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
+		Bucket: pointerOrNil(bucket),
+		Key:    pointerOrNil(key),
 	})
+}
+
+func pointerOrNil[T comparable](v T) *T {
+	var zero T
+	if v == zero {
+		return nil
+	}
+	return &v
 }
